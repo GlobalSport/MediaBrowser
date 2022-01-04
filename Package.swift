0cbcdcb10acf84bb88cd74d38c7f29af55feb49f
jean torres <j.torres@mycoachsport.com>
2022-01-04 17:43:13 +0100

    update package

--- Package.swift
+++ Package.swift
@@ -2,5 +2,25 @@
 import PackageDescription
 
 let package = Package(
-    name: "MediaBrowser"
+    name: "MediaBrowser",
+    platforms: [
+        .iOS(.v12),
+        .macOS(.v10_12)
+        ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "MediaBrowser",
+            targets: ["MediaBrowser"]),
+    ],
+    dependencies: [],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "MediaBrowser",
+            dependencies: [],
+            resources: [.copy("Sources/MediaBrowser/MediaBrowser.xcassets")]
+        )
+    ]
 )
